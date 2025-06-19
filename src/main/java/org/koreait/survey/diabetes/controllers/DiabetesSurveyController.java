fa294971979d8c78b40460ad79c86ebe72f1cd1e
leesomin1204 <sm1157@naver.com>
2025-06-19 09:58:26 +0900

    .

--- src/main/java/org/koreait/survey/diabetes/controllers/DiabetesSurveyController.java
+++ src/main/java/org/koreait/survey/diabetes/controllers/DiabetesSurveyController.java
@@ -2,12 +2,17 @@ package org.koreait.survey.diabetes.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.koreait.global.constants.Gender;
 import org.koreait.global.libs.Utils;
+import org.koreait.survey.diabetes.constants.SmokingHistory;
+import org.koreait.survey.diabetes.services.DiabetesSurveyService;
+import org.koreait.survey.diabetes.validators.DiabetesSurveyValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.List;
 
@@ -18,6 +23,8 @@ import java.util.List;
 public class DiabetesSurveyController {
 
     private final Utils utils;
+    private final DiabetesSurveyValidator validator;
+    private final DiabetesSurveyService surveyService;
 
     @ModelAttribute("addCss")
     public List<String> addCss() {
@@ -26,7 +33,21 @@ public class DiabetesSurveyController {
 
     @ModelAttribute("requestDiabetesSurvey")
     public RequestDiabetesSurvey requestDiabetesSurvey() {
-        return new RequestDiabetesSurvey();
+        RequestDiabetesSurvey form = new RequestDiabetesSurvey();
+        form.setGender(Gender.FEMALE);
+        form.setSmokingHistory(SmokingHistory.CURRENT);
+
+        return form;
+    }
+
+    @ModelAttribute("genders")
+    public Gender[] genders() {
+        return Gender.values();
+    }
+
+    @ModelAttribute("smokingHistories")
+    public SmokingHistory[] smokingHistories() {
+        return SmokingHistory.values();
     }
 
     @GetMapping("/step1")
@@ -39,6 +60,8 @@ public class DiabetesSurveyController {
     @PostMapping("/step2")
     public String step2(@Valid RequestDiabetesSurvey form, Errors errors, Model model) {
         commonProcess("step", model);
+
+        validator.validate(form, errors);
 
         if (errors.hasErrors()) {
             return utils.tpl("survey/diabetes/step1");
@@ -56,12 +79,20 @@ public class DiabetesSurveyController {
      * @return
      */
     @PostMapping("/process")
-    public String process(@Valid RequestDiabetesSurvey form, Errors errors, Model model) {
+    public String process(@Valid RequestDiabetesSurvey form, Errors errors, Model model, SessionStatus status) {
         commonProcess("step", model);
+
+        validator.validate(form, errors);
 
         if (errors.hasErrors()) {
             return utils.tpl("survey/diabetes/step2");
         }
+
+        // 설문 결과 및 저장 처리
+        surveyService.process(form);
+
+        // 처리 완료 후 세션값으로 더이상 변경되지 않도록 완료 처리
+        status.setComplete();
 
         return "redirect:/survey/diabetes/result/설문번호";
     }
